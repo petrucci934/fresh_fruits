4ae334ace418b4fa44fd588381158edb329fe517
Petrucci Lucena <petrucci.lucenaplc@gmail.com>
2025-01-14 13:06:21 +0000

    add assets/Hero/utility and animation

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Navbar/Hero/Hero'
 
 const App=()=> {
   return (
     <>
       <main className='overflow-x-hidden'>
         <Navbar/>
+        <Hero/>
       </main>
     </>
   )
---
+++ my-project/src/utility/animation.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+export const FadeUp = (delay) => {
+  return {
+    hidden:{
+        opacity:0,
+        y:100,
+    },
+    visible:{
+        opacity:1,
+        x:0,
+        transition:{
+            duration:1,
+            delay: delay,
+        }
+    }
+  }
+}
+
+
+export const FadeLeft = (delay) => {
+    return {
+      hidden:{
+          opacity:0,
+          x:100,
+      },
+      visible:{
+          opacity:1,
+          x:0,
+          transition:{
+              duration:1,
+              delay: delay,
+          }
+      }
+    }
+  }
+
+
+export const FadeRight = (delay) => {
+    return {
+      hidden:{
+          opacity:0,
+          x:-100,
+      },
+      visible:{
+          opacity:1,
+          x:0,
+          transition:{
+              duration:1.3,
+              delay: delay,
+          }
+      }
+    }
+  }
